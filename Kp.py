997497b5b1acada03733b9582b3f06d47d15eba9
frnxx <144995075+phrnxx@users.noreply.github.com>
2025-04-02 10:48:03 +0300

    Kp.py

--- Kp.py
+++ Kp.py
@@ -1,12 +1,10 @@
-# Імпорт бібліотек
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from deap import base, creator, tools, algorithms
-import knapsack  # власний модуль (має бути в робочій директорії)
+import knapsack  
 
-# Налаштування параметрів
 POPULATION_SIZE = 50
 P_CROSSOVER = 0.9
 P_MUTATION = 0.1
@@ -14,7 +12,6 @@ MAX_GENERATIONS = 50
 RANDOM_SEED = 42
 random.seed(RANDOM_SEED)
 
-# Ініціалізація DEAP
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
@@ -23,7 +20,6 @@ toolbox.register("zeroOrOne", random.randint, 0, 1)
 toolbox.register("individualCreator", tools.initRepeat, creator.Individual, toolbox.zeroOrOne, len(knapsack.Knapsack01Problem()))
 toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
 
-# Функція придатності
 def knapsackValue(individual):
     return knapsack.Knapsack01Problem().getValue(individual),
 
@@ -32,7 +28,6 @@ toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", tools.cxOnePoint)
 toolbox.register("mutate", tools.mutFlipBit, indpb=1.0/len(knapsack.Knapsack01Problem()))
 
-# Генетичний алгоритм
 population = toolbox.populationCreator(n=POPULATION_SIZE)
 hof = tools.HallOfFame(1)
 stats = tools.Statistics(lambda ind: ind.fitness.values)
@@ -44,7 +39,6 @@ population, logbook = algorithms.eaSimple(
     ngen=MAX_GENERATIONS, stats=stats, halloffame=hof, verbose=True
 )
 
-# Візуалізація
 maxFitnessValues, meanFitnessValues = logbook.select("max", "avg")
 sns.set_style("whitegrid")
 plt.plot(maxFitnessValues, color='red', label='Max')
